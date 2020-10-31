94641d97f28194d22a116d93a244d2290965e9e9
Jose Cuentasail <jcuentast@gmail.com>
2020-10-31 16:37:05 -0500

    feat: create project

---
+++ app/src/main/java/com/josecuentas/androidintents/features/main/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.josecuentas.androidintents.features.main.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.josecuentas.androidintents.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
